c30fb32800d156acdd4424f507d42f9694ba7de0
Aria Gomes <aria.gomes00@gmail.com>
2023-02-02 08:20:27 -0500

    Proof of concept, conceptualize UI

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,ts,tsx}"],
+  darkMode: 'class',
+  lightMode: 'class',
+  theme: {
+    extend: {},
+    colors: {
+      tb: '#000',
+      tw: '#FFF'
+    }
+  },
+  plugins: [
+    require('flowbite/plugin')
+  ],
+}
